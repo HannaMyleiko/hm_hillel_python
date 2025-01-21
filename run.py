7de40095650323dae4c3bad7e5e984dce58ffdda
HannaMyleiko <hannamyleiko@gmail.com>
2025-01-20 16:06:33 -0800

    Homework 05. - Price class - CurrencyExchange class with convertion based on MIDDLE currency - Journal project with auth decorator on management commands

--- run.py
+++ run.py
@@ -10,59 +10,66 @@ The class represents currency exchange rates based for the abstract unit: CHF
 """
 class CurrencyExchange:
     rates = {
-        "usd": 0.9,
-        "chf": 1,
-        "uah": 0.03,
-        "eur": 0.85,
-        "gbp": 1.1
+        "usd": {
+            "usd": 1,
+            "uah": 40,
+            "eur": 0.95,
+            "chf": 0.85,
+            "gbp": 0.9
+        },
+        "chf": {
+            "usd": 1.3,
+            "uah": 50,
+            "eur": 1.2,
+            "chf": 1,
+            "gbp": 1.05
+        },
+        "uah": {
+            "usd": 0.02,
+            "uah": 1,
+            "eur": 0.015,
+            "chf": 0.013,
+            "gbp": 0.014
+        },
+        "eur": {
+            "usd": 1.1,
+            "uah": 45,
+            "eur": 1,
+            "chf": 0.95,
+            "gbp": 0.97
+        },
+        "gbp": {
+            "usd": 1.2,
+            "uah": 47,
+            "eur": 1.15,
+            "chf": 0.95,
+            "gbp": 1
+        }
     }
 
+    CONVERTER_BASE = "chf"
+
     @classmethod
-    def convert(cls, value: float, from_: str, to_: str) -> float:
+    def convert(cls, value: float, from_: str, to_: str) -> "Price":
         try:
-            from_rate = cls.rates[from_]
-            to_rate = cls.rates[to_]
-            result = from_rate / to_rate
-            return result * value
+            coef = cls.rates[from_.lower()][to_.lower()]
+            result = Price(value = value * coef, currency=to_.lower())
+            return result
         except Exception as error:
             raise CurrencyError() from error
 
-def auth(func):
-    user_list = {
-        "ann": "qwer1234",
-        "admin": "admin"
-    }
-    def user_auth():
-        global CACHE
-        while (len(CACHE)==0):
-            try:
-                username = input("User Name: ")
-                password = input("Password: ")
-                if user_list[username]==password:
-                    CACHE.append(username)
-            except Exception:
-                pass
-
-    def wrapper(*args, **kwargs):
-        user_auth()
-        func(*args, **kwargs)
-    return wrapper
-
 class Price:
-    @auth
+
     def __init__(self, value: float, currency: str):
         self.value: float = value
-        self.currency: str = currency
+        self.currency: str = currency.lower()
 
     def __str__(self) -> str:
         return f"Price: {self.value} {self.currency}"
 
-    def adjust(self, other: "Price") -> "Price":
-        return Price(value=CurrencyExchange.convert(value=other.value, from_=other.currency,to_=self.currency), currency=self.currency)
-
     def __add__(self, other: Any) -> "Price":
         if not isinstance(other, Price):
-            raise ValueError("Can perform operation only with Pirces objects")
+            raise ValueError("Can perform operation only with Prices objects")
         else:
             if self.currency != other.currency:
                 other = self.adjust(other)
@@ -70,12 +77,27 @@ class Price:
 
     def __sub__(self, other: Any) -> "Price":
         if not isinstance(other, Price):
-            raise ValueError("Can perform operation only with Pirces objects")
+            raise ValueError("Can perform operation only with Prices objects")
         else:
             if self.currency != other.currency:
                 other = self.adjust(other)
             return Price(value=self.value - other.value, currency=self.currency)
 
+    """
+    Converts Price to the specified currency directly
+    """
+    def convert(self, to_: str) -> "Price":
+        result = CurrencyExchange.convert(value=self.value, from_=self.currency, to_=to_)
+        return result
+
+    """
+    Adjusts the specified Price to the current Price's currency using a middle currency (specified in CurrencyExchange.CONVERTER_BASE)
+    """
+    def adjust(self, other: "Price") -> "Price":
+        result = other.convert(CurrencyExchange.CONVERTER_BASE).convert(self.currency)
+        return result
+
+# Test run
 phone = Price(value=200, currency="usd")
 tablet = Price(value=400, currency="uah")
 tv = Price(value=300, currency="eur")
